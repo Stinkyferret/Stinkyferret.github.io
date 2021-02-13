e400e954be0b347efefa877a529c01d7eb0344a3
Maaren <maaren.vandesijpe@student.hogent.be>
2021-02-13 12:03:03 +0100

    Add project files.

---
+++ testform/contactForm.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+if (isset($_POST['submit'])) {
+
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$name = $_POST['name'];
+$mailFrom = $_POST['mail'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$mailTo = "maaren.vandesijpe@student.hogent.be";
+$headers = "Van: ".$mailFrom;
+$txt = "U heeft een e-mail ontvangen van ".$name.".\n\n".$message;
+
+
+mail($mailTo, $subject, $txt, $headers);
+header("Location: index.php?mailsend");
+
+
+
+
+
+
+
+
+
+}
